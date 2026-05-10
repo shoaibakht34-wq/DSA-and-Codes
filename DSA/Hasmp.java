831cb53798c39b85ee2431e5a1d9b99a1aa7d177
Shoaib Akhtar <shoaibakht34@gmail.com>
2026-05-10 15:10:35 +0530

    Add initial implementation of Hasmp class with HashMap example

---
+++ DSA/Hasmp.java
@@ -0,0 +1,11 @@
+package DSA;
+import java.util.HashMap;
+public class Hasmp {
+    public static void main(String[] args) {
+        HashMap<String , Integer> map=new HashMap<>();
+        map.put("Shoaib", 1);
+        map.put("Aditya", 19);
+        map.put("Akhtar",12 );
+        System.out.println(map+" "+map.size() );
+    }
+}
